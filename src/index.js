ef7a0f824d92f2c13a47f87d0403b79a95dc9439
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-14 11:29:47 -0300

    guardado hasta la coneccion a la base de datos porque esta caido mongoDB

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//requires
+const express = require("express");
+const morgan = require("morgan");
+const app=express();
+const path = require("path")
+
+
+//settings
+app.set("port",process.env.PORT || 3000)
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+//routes
+app.use("/api/tasks", require("./routes/task.routes"))
+
+
+//static files
+app.use(express.static(path.join(__dirname,"public")))
+
+//startin the server
+app.listen(app.get("port"),()=>{
+    console.log(`Server on port ${app.get("port")}`);
+})
+
