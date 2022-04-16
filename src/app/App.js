6e9a611ca41c31dff1d84277f23473083cbd9e9e
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-16 10:09:21 -0300

    creado formulario

--- src/app/App.js
+++ src/app/App.js
@@ -5,11 +5,38 @@ class App extends Component{
     render(){
         return(
             <div>
+                {/*navigation */}
                 <nav className ="light-blue darken-4">
                     <div className ="container">
                         <a className="brand-logo" href="/">MERN Stack</a>
                     </div>
                 </nav>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                    <form>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input type="text" placeholder="Task Title">
+                                                </input>
+                                            </div>
+                                            <div className="input-field col s12">
+                                                <textarea placeholder="Task description" className="materialize-textarea">
+                                                </textarea>
+                                            </div>
+                                            <button type="submit" className="btn light-blue darken-4">Send</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s7">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
